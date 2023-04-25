2c390eeb3a26d8d314e929c337c88b65389506d1
Jannis Klinkenberg <j.klinkenberg@itc.rwth-aachen.de>
2023-04-25 13:16:03 +0200

    get rid of hard coded paths

--- run_file.sh
+++ run_file.sh
@@ -5,6 +5,4 @@
 python3 -W ignore tag_program  \
 --global_batch_size=tag_batch \
 --augment=tag_aug \
---image_dir=${WORK}/repos/distributed-ml/pytorch/data/images_collective \
---mask_dir=${WORK}/repos/distributed-ml/pytorch/data/masks_collective \
 --epoch=tag_epoch 2>&1
